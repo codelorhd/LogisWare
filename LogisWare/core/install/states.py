34f0cfe7481c72f329d2a79928e2ac8da660dc55
ABOYEJI SOLOMON ADELEKE <solesty7@gmail.com>
2019-08-02 15:59:21 +0100

    Fix bug: States.py

--- LogisWare/core/install/states.py
+++ LogisWare/core/install/states.py
@@ -3,25 +3,33 @@ from core.models import State
 import json
 import os
 
-path =  os.path.dirname(
-    os.path.dirname( os.path.abspath( __name__ ) )
-    )
+path = os.path.dirname(
+    os.path.dirname(os.path.abspath(__name__))
+)
 
 path = path + "/LogisWare/core/install/NIGERIA_STATES_AND_LGS.json"
 
-with open( path ) as json_file:  
+with open(path) as json_file:
     json_data = json.load(json_file)
 
     for data in json_data:
-        print( )
+        print()
         state_name = data['state']['name']
-        print( state_name )
+        print(state_name)
+
         try:
-            State.objects.get( name = state_name )
-        except State.DoesNotExist:
+            State.objects.get(name=state_name)
+        except Exception:
             state_model = State()
             state_model.name = state_name
             state_model.save()
+
+        # try:
+        #     State.objects.get( name = state_name )
+        # except State.DoesNotExist:
+        #     state_model = State()
+        #     state_model.name = state_name
+        #     state_model.save()
 
             # state_locals = data['state']['locals']
             # print("---------------")
